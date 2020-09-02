8c25374f2c926cb9ae21fe94ec9f78ed35331cc4
SergiiKov <yamatsuba008@gmail.com>
2020-09-02 15:41:10 +0300

    add card

--- src/pages/about/about-page.jsx
+++ src/pages/about/about-page.jsx
@@ -74,11 +74,11 @@ const AboutPage = () => {
         </div>
       </div>
 
-<div className="row">
+<div className="row m-b-2">
   <div className="col-1-of-3">
     <div className="card">
       <div className="card__side card__side--front">
-        <div className="card__picture card__picture--3">
+        <div className="card__picture card__picture--1">
            &nbsp;
         </div>
          <h4 className="card__heading">
@@ -104,7 +104,69 @@ const AboutPage = () => {
                                 </div>
                             </div>
                        </div>
-                    </div>
+                       </div>
+                       <div className="col-1-of-3">
+                       <div className="card">
+      <div className="card__side card__side--front">
+        <div className="card__picture card__picture--2">
+           &nbsp;
+        </div>
+         <h4 className="card__heading">
+           <span className="card__heading-span card__heading-span--1">The Sea Explorer</span>
+          </h4>
+           <div className="card__details">
+                                    <ul>
+                                        <li>3 day tours</li>
+                                        <li>Up to 30 people</li>
+                                        <li>2 tour guides</li>
+                                        <li>Sleep in cozy hotels</li>
+                                        <li>Difficulty: easy</li>
+                                    </ul>
+                                </div>
+                           </div>
+                           <div className="card__side card__side--back card__side--back-2">
+                                <div className="card__cta">
+                                    <div className="card__price-box">
+                                        <p className="card__price-only">Photo</p>
+                                        <p className="card__price-value">Travel</p>
+                                    </div>
+                                    <a href="#popup" class="btn btn--white">Book now!</a>
+                                </div>
+                            </div>
+                       </div>
+                       </div>
+                       <div className="col-1-of-3">
+                       <div className="card">
+      <div className="card__side card__side--front">
+        <div className="card__picture card__picture--3">
+           &nbsp;
+        </div>
+         <h4 className="card__heading">
+           <span className="card__heading-span card__heading-span--1">The Sea Explorer</span>
+          </h4>
+           <div className="card__details">
+                                    <ul>
+                                        <li>3 day tours</li>
+                                        <li>Up to 30 people</li>
+                                        <li>2 tour guides</li>
+                                        <li>Sleep in cozy hotels</li>
+                                        <li>Difficulty: easy</li>
+                                    </ul>
+                                </div>
+                           </div>
+                           <div className="card__side card__side--back card__side--back-3">
+                                <div className="card__cta">
+                                    <div className="card__price-box">
+                                        <p className="card__price-only">Photo</p>
+                                        <p className="card__price-value">Travel</p>
+                                    </div>
+                                    <a href="#popup" class="btn btn--white">Book now!</a>
+                                </div>
+                            </div>
+                       </div>
+                       </div>
+
+                  
                  </div>
     </React.Fragment>
   
--- src/pages/projects/projects-page.jsx
+++ src/pages/projects/projects-page.jsx
@@ -19,7 +19,7 @@ const ProjectPage = () => {
      <div>
      
     <h3 className='heading-tertiary'>Todo List</h3>
-    <a href='https://github.com/SergiiKov/My-ToDo-List' target="_blank" rel="noopener noreferrer">
+    <a className='style-mail__link' href='https://github.com/SergiiKov/My-ToDo-List' target="_blank" rel="noopener noreferrer">
                     <div className='project-card-container'>
                         <img src={require(`../../img/todolist.png`)} alt='todolist' className='img-projects' />
                             <div className='projects-text-container'>
@@ -35,7 +35,7 @@ const ProjectPage = () => {
          <div className='projects-block' >
      <div >
     <h3 className='heading-tertiary'>Robot-page</h3>
-    <a href='https://github.com/SergiiKov/cards-friends' target="_blank" rel="noopener noreferrer">
+    <a className='style-mail__link' href='https://github.com/SergiiKov/cards-friends' target="_blank" rel="noopener noreferrer">
                     <div className='project-card-container'>
                         <img src={require(`../../img/robot-friends.png`)} alt='robot-friends' className='img-projects' />
                             <div className='projects-text-container'>
@@ -51,7 +51,7 @@ const ProjectPage = () => {
    <div className='projects-block' >
      <div>
     <h3 className='heading-tertiary'>VN Create</h3>
-    <a href='https://github.com/SergiiKov/vncreate' target="_blank" rel="noopener noreferrer">
+    <a className='style-mail__link' href='https://github.com/SergiiKov/vncreate' target="_blank" rel="noopener noreferrer">
                     <div className='project-card-container'>
                         <img src={require(`../../img/vncreate-1.png`)} alt='VN Create' className='img-projects' />
                         <img src={require(`../../img/vncreate-2.png`)} alt='VN Create' className='img-projects' />
@@ -66,7 +66,7 @@ const ProjectPage = () => {
   <div className='projects-block' >
      <div>
     <h3 className='heading-tertiary'>Smp.vin.ua</h3>
-    <a href='http://smp.vin.ua/joomla/' target="_blank" rel="noopener noreferrer">
+    <a className='style-mail__link' href='http://smp.vin.ua/joomla/' target="_blank" rel="noopener noreferrer">
      <div className='project-card-container'>
                         <img src={require(`../../img/smp.png`)} alt='smp.vin.ua' className='img-projects' />
                             <div className='projects-text-container'>
@@ -82,7 +82,7 @@ const ProjectPage = () => {
    <div className='projects-block'>
      <div>
     <h3 className='heading-tertiary'>Main-page iac</h3>
-    <a href='http://smp.vin.ua/joomla/' target="_blank" rel="noopener noreferrer">
+    <a className='style-mail__link' href='http://smp.vin.ua/joomla/' target="_blank" rel="noopener noreferrer">
     <div className='project-card-container'>
                         <img src={require(`../../img/main-page.png`)} alt='main-page iac' className='img-projects' />
                             <div className='projects-text-container'>
