4bfe870bc263f3462f4ec93919d63864b9dbad5e
SergiiKov <yamatsuba008@gmail.com>
2020-09-15 14:34:40 +0300

    add test menu

--- src/component/burger-menu/burger-menu.jsx
+++ src/component/burger-menu/burger-menu.jsx
@@ -27,7 +27,7 @@ const Hamburger = () => {
         <ul className="hamburger-ul">
           {routes.map((l) => (
             <li key={l.label}>
-              <Link to={l.path} onClick={() => setOpen(!open)}>
+              <Link className='nav__link' to={l.path} onClick={() => setOpen(!open)}>
                 <h3 className={l.index && 'index-li'}>{l.label}</h3>
               </Link>
             </li>
